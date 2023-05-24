05c37c928f53ca1fefa7fe378ac2526cb38a1ad1
Tim Saes - PS172191 <PS172191@edu.summacollege.nl>
2023-05-24 15:41:00 +0200

    Music laravel project

---
+++ Music/resources/views/base.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="XA-UA-Compatible" content="ie=edge">
+    <title>Music Database</title>
+    @vite('resources/css/app.css')
+
+    <style>
+        * {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    @yield('main')
+</body>
+
+</html>
